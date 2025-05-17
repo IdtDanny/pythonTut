88aabe1b6d86d94545bd1453718d55d722f7171b
Danny I <idtdanny@gmail.com>
2025-05-17 09:15:01 +0200

    Input + lists

---
+++ input.py
@@ -0,0 +1,6 @@
+word = input("Enter your name: ")
+
+print("Hello, " + word)
+
+# Or
+# print(f"Hello, {word}")
---
+++ lists.py
@@ -0,0 +1,7 @@
+myList = ["Danny", "Alice", "Mugisha"]
+
+myList.append("Danton")
+
+myList.sort()
+
+print(myList)
